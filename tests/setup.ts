27d59505e04cceb79db0be938468ade68c925c11
nomadEngineer-FUK <s.hiraoka08@gmail.com>
2024-11-27 10:38:19 +0900

    modify setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,8 +1,13 @@
-// tests/setup.ts
+import { setup } from '@nuxt/test-utils';
 import { TextEncoder, TextDecoder } from 'util';
 
+// Nuxtのテスト環境を設定
+setup({
+    server: false, // サーバーを起動せずテストを実行
+});
+
+// TextEncoderとTextDecoderの設定
 if (typeof (global as any).TextEncoder === 'undefined') {
     (global as any).TextEncoder = TextEncoder;
     (global as any).TextDecoder = TextDecoder;
 }
-
